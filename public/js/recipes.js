f9517167cabe9da3369420bf2bfb46d698f3b7ab
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-06-08 07:01:51 -0700

    Recipe App Working

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -14,13 +14,22 @@ $("#recipeSearch").on("click", function(event) {
     console.log("recipes.js", data);
 
     //console.log("Recipe Id", data.results[0].id);
+    var newRecipeID = {
+      id: data.results[0].id
+    };
+
+    console.log("Recipe Id", newRecipeID);
+    
+    $.post("/api/recipe/info", newRecipeID).then(function(data) {
+      console.log("1st Recipe Info", data);
+
+      
+    });
   });
 
   var newRecipeID = {
     id: ""
   }
 
-  $.post("/api/recipe/info", newRecipeID).then(function(data) {
-    console.log("1st Recipe Info", data);
-  });
+  
 });
